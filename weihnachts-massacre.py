fd0613b0d5c8d609c36c9855b0d67caeb37bb869
Stig <simon@mustig.se>
2020-12-03 19:51:22 +0100

    Dag 2 git gud

--- weihnachts-massacre.py
+++ weihnachts-massacre.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
+import re
+
 def indatakrossen(fn):
-  with open("data/{:s}".format(fn), "r") as file:
-    return [l for l in file.readlines()]
+  with open("data/{:s}".format(fn), "r") as filbunke:
+    return [l.rstrip() for l in filbunke.readlines()]
 
 def everscream():
   representationskontot = [int(n) for n in indatakrossen("1a.txt")]
@@ -10,10 +12,24 @@ def everscream():
   # Lol brute force orka krångla
   print([a*d*e for a in representationskontot for d in representationskontot[::-1] for e in representationskontot if a+d+e == 2020][0])
 
+def haigography_of_saint_nicholaus():
+  rules, passwords = zip(*[s.split(': ') for s in indatakrossen("2.txt")])
+
+  def hard_presents(r):
+    return r.replace("-", " ").split(" ")
+
+  bag_o_presents = list(zip(map(hard_presents, rules), passwords))
+  print(sum(1 for p, q in bag_o_presents if int(p[0]) <= q.count(p[2]) <= int(p[1])))
+  print(sum(1 for p, q in bag_o_presents if (p[2] in q[int(p[0])-1]) ^ (p[2] in q[int(p[1])-1])]))
+
+def de_anima():
+  pass
 
 if __name__ == "__main__":
   problems = {
     1: everscream,
+    2: haigography_of_saint_nicholaus,
+    3: de_anima,
   }
 
-  problems[1]()
+  problems[3]()
