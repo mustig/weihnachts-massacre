922d41b7ce72cb3dd55b2e58d32800330cb7216a
Stig <simon@mustig.se>
2020-12-05 21:25:14 +0100

    Dag 4

--- weihnachts-massacre.py
+++ weihnachts-massacre.py
@@ -37,12 +37,32 @@ def de_anima():
     backar = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
     print(np.product([kraschtest(*backe) for backe in backar]))
 
+def malfunctioning_reindeer_mech():
+    with open("data/4.txt", "r") as filbunke:
+        passports = ''.join([l for l in filbunke.readlines()]).split("\n\n")
+    passports = [dict(fields.split(':') for fields in p.split()) for p in passports]
+    fuck_cid = [{k: v for k, v in passport.items() if k != 'cid'} for passport in passports]
+    n_valid_passports = sum(1 for test in fuck_cid if len(test.keys()) == 7)
+    print(n_valid_passports)
+    tests = {
+        'byr': lambda n: 1920 <= int(n) <= 2002,
+        'iyr': lambda n: 2010 <= int(n) <= 2020,
+        'eyr': lambda n: 2020 <= int(n) <= 2030,
+        'hgt': lambda n: (150 <= int(n[:n.find('cm')]) <= 193) if('cm' in n) else ((59 <= int(n[:n.find('in')]) <= 76) if('in' in n) else False),
+        'hcl': lambda n: len(n) == 7 and re.search(r'#[0-9a-f]{6}', n) is not None,
+        'ecl': lambda n: len(n) == 3 and n in ("amb", "blu", "brn", "gry", "grn", "hzl", "oth"),
+        'pid': lambda n: len(n) == 9 and re.search(r'[0-9]{9}', n) is not None
+    }
+
+    print(sum(sum(tests[k](v) for k, v in p.items()) == 7 for p in fuck_cid))
+
 
 if __name__ == "__main__":
     problems = {
         1: everscream,
         2: hagiography_of_saint_nicholaus,
         3: de_anima,
+        4: malfunctioning_reindeer_mech
     }
 
     problems[datetime.datetime.today().day]()
